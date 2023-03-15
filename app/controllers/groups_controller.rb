65b1ddb14be864de36d0550c38dfdb0e6892f678
johan <t511069160@qq.com>
2023-03-15 17:08:24 +0800

    创建新增帖子#create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,21 @@ class GroupsController < ApplicationController
         @groups = Group.all
     end
 
+    # 新增帖子页面
     def new
         @group = Group.new
     end
+
+    # 新增帖子 POST
+    def create
+        @group = Group.new(group_params)
+        @group.save
+
+        redirect_to groups_path
+    end
+
+    private
+        def group_params
+            params.require(:group).permit(:title, :description)
+        end
 end
