f8e0ebbd32bb09b2ef6cc720265f9f4533bf85e8
johan <t511069160@qq.com>
2023-03-15 16:22:40 +0800

    将首页换成组index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'welcome#index'
+  # root 'welcome#index'
+  root 'groups#index'
 
   resources :groups
 end
