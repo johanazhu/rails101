e85c8bea6a1fcec6da5df80d9d050d94ede77df5
johan <t511069160@qq.com>
2023-06-30 16:53:34 +0800

    添加域名

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Rails101
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.hosts << "rails101.zeabur.app"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
